712304eeb0b5913baf1ce17338bfdea1bef073d0
DavidBanda <davidbandag96@gmail.com>
2020-04-06 00:18:25 -0600

    generate js class components

--- coronavirus-sofe/app/routes/index.js
+++ coronavirus-sofe/app/routes/index.js
@@ -28,6 +28,20 @@ export default class IndexRoute extends Route {
       perMillion += current[i].casesPerOneMillion;
     }
 
+    function sortResults(prop, asc) {
+      current.sort(function(a, b) {
+          if (asc) {
+              return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+          } else {
+              return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+          }
+      });
+    }
+
+    sortResults('cases', false)
+
+    console.log(current)
+
 
     return { data: [
       {
